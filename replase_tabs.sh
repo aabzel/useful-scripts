5576babdd82e2ae7143c821e59f36dde8f29bb81
Alexander Barunin <aabzel@yandex.ru>
2020-10-28 00:00:06 +0300

    Update replase_tabs.sh

--- replase_tabs.sh
+++ replase_tabs.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 " ">abs_path_files_with_tabs.txt
-#(find . -type f -name "*.c" -exec grep -l -P "\t" {} +)
-
-(find . -type f -name "*convert*.c" -exec grep -l -P "\t" {} +) >>  abs_path_files_with_tabs.txt
-(find . -type f -name "*shell*.c" -exec grep -l -P "\t" {} +) >>  abs_path_files_with_tabs.txt
-(find . -type f -name "*board*.c" -exec grep -l -P "\t" {} +) >>  abs_path_files_with_tabs.txt
-(find . -type f -name "*lin*.c" -exec grep -l -P "\t" {} +) >>  abs_path_files_with_tabs.txt
-(find . -type f -name "*uio*.c" -exec grep -l -P "\t" {} +) >>  abs_path_files_with_tabs.txt
-(find . -type f -name "*tja1021*.c" -exec grep -l -P "\t" {} +) >>  abs_path_files_with_tabs.txt
-(find . -type f -name "*tic124*.c" -exec grep -l -P "\t" {} +) >>  abs_path_files_with_tabs.txt
-(find . -type f -name "*ksz8081*.c" -exec grep -l -P "\t" {} +) >>  abs_path_files_with_tabs.txt
-(find . -type f -name "*ad56*.c" -exec grep -l -P "\t" {} +) >>  abs_path_files_with_tabs.txt
-(find . -type f -name "*lin*.c" -exec grep -l -P "\t" {} +) >>  abs_path_files_with_tabs.txt
+(find . -type f -name "*.c" -exec grep -l -P "\t" {} +) >>  abs_path_files_with_tabs.txt
+(find . -type f -name "*.h" -exec grep -l -P "\t" {} +) >>  abs_path_files_with_tabs.txt
 
 while IFS='' read -r LINE || [ -n "${LINE}" ]; do
     sed -i 's/\t/    /g'  "${LINE}" 
